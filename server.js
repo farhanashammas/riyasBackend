89d32a104dc84a0774e0e2d0f6805ba0af1eb54f
farhana <farhanamananthery@gmail.com>
2020-01-08 13:40:57 +0530

    final

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ app.use('/login',loginRouter);
 app.use('/products',productRouter);
 
 
-process.on('uncaughtException', (err) => {
-    console.error('There was an uncaught error', err)
-    process.exit(1) //mandatory (as per the Node docs)
-})
+// process.on('uncaughtException', (err) => {
+//     console.error('There was an uncaught error', err)
+//     process.exit(1) //mandatory (as per the Node docs)
+// })
 
 app.listen(port, () => {
     // console.log(__dirname)
