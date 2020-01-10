8021dbaedadb2ef8fea20701accd092e730c36a0
farhana <farhanamananthery@gmail.com>
2020-01-10 17:12:04 +0530

    notification

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-const userAuth = require('./src/routers/userAuth');
+
 
 mongoose.Promise=global.Promise;
 
@@ -20,12 +20,12 @@ app.use(express.static(path.join(__dirname, "/public")));
 const signupUserRouter= require('./src/routers/signupUserRouter')();
 const loginRouter= require('./src/routers/loginRouter')();
 const productRouter=require('./src/routers/productRouter')();
-var { noteModel } = require('./src/models/noteModel');
-const auth = require('./src/routers/auth');
+const noteRouter=require('./src/routers/noteRouter')();
 
 app.use('/signupUser',signupUserRouter);
 app.use('/login',loginRouter);
 app.use('/products',productRouter);
+app.use('/note',noteRouter);
 
 const uri="mongodb+srv://farhana:farhana@cluster0-o93hy.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(uri);
@@ -40,59 +40,6 @@ mongoose.connection.on('connected', function() {
         console.log('Connection to Mongo established.')
     });
 
-
-        //Admin & user
-        app.route('/note')
-        .post(auth.required, (req, res) => {
-            res.header("Access-Control-Allow-Origin", "*")
-            res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-            userAuth(req.body.userId).then((user) => {
-                if (user == "admin" || user== "user") {
-                    // console.log("noteeeeeeeeeeeeeeeeeee")
-                    noteModel.find((err, result) => {
-                        // var data=(result[0].note)
-                        // console.log(result)
-                        if (err) 
-                            return res.json({ Status: "Error" });
-                        else
-                            return res.json( result[0].note);
-                    });
-                }
-                else 
-                    return res.json({ Status: "Error" });
-            }).catch((err) => {
-                res.json({ Status: "Error" });
-            });
-        });
-
-        //Admin
-    app.route('/newNote')
-    .post(auth.required, (req, res) => {
-        console.log("aut")
-        res.header("Access-Control-Allow-Origin", "*")
-        res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        userAuth(req.body.userId).then((user) => {
-            console.log(user)
-            if (user == "admin") {
-                console.log("admin aan")
-                var note = req.body
-                var note = new noteModel(note);
-                console.log("note to add"+note)
-                // '5e17272232f80d11741cff58',{ $set : {note:note.note } },
-                noteModel.findByIdAndUpdate('5e17272232f80d11741cff58',{ $set : {note:note.note } }, (err, result) => {
-                    if (err) 
-                        return res.json({ Status: "save Error" });
-                    else
-                        return res.json({ Status: "Success" });
-                });
-            }
-            else 
-                return res.json({ Status: "admin Error" });
-        }).catch((err) => {
-            res.json({ Status: "catch Error" });
-        });
-    });
-    
 
 process.on('uncaughtException', (err) => {
     console.error('There was an uncaught error', err)
---
+++ src/routers/noteRouter.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var noteRouter = express.Router();
+var { noteModel } = require('../models/noteModel');
+const userAuth = require('./userAuth');
+const auth = require('./auth');
+
+function route() {
+        //Admin & user
+        noteRouter.route('/')
+        .post(auth.required, (req, res) => {
+            res.header("Access-Control-Allow-Origin", "*")
+            res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+            userAuth(req.body.userId).then((user) => {
+                if (user == "admin" || user== "user") {
+                    // console.log("noteeeeeeeeeeeeeeeeeee")
+                    noteModel.find((err, result) => {
+                        // var data=(result[0].note)
+                        // console.log(result)
+                        if (err) 
+                            return res.json({ Status: "Error" });
+                        else
+                            return res.json( result[0].note);
+                    });
+                }
+                else 
+                    return res.json({ Status: "Error" });
+            }).catch((err) => {
+                res.json({ Status: "Error" });
+            });
+        });
+
+        //Admin
+    noteRouter.route('/newNote')
+    .post(auth.required, (req, res) => {
+        console.log("aut")
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        userAuth(req.body.userId).then((user) => {
+            console.log(user)
+            if (user == "admin") {
+                console.log("admin aan")
+                var note = req.body
+                var note = new noteModel(note);
+                console.log("note to add"+note)
+                // '5e17272232f80d11741cff58',{ $set : {note:note.note } },
+                noteModel.findByIdAndUpdate('5e17272232f80d11741cff58',{ $set : {note:note.note } }, (err, result) => {
+                    if (err) 
+                        return res.json({ Status: "save Error" });
+                    else
+                        return res.json({ Status: "Success" });
+                });
+            }
+            else 
+                return res.json({ Status: "admin Error" });
+        }).catch((err) => {
+            res.json({ Status: "catch Error" });
+        });
+    });
+    
+    return noteRouter;
+
+}
+module.exports = route;
+
+
